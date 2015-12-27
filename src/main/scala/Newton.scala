b7a28ee09d6d0db47f217736f04cc06d1776dca1
vstostap <vstostap@gmail.com>
2015-12-27 20:56:09 +0200

    Isaac Newton sqrt algorithm

---
+++ src/main/scala/Newton.scala
@@ -0,0 +1,20 @@
+import scala.annotation.tailrec
+
+object Newton {
+  /**
+    * Isaac Newton's sqrt algorithm
+    * @param x <Double> value
+    * @return square root of value
+    */
+    private def solve(x: Double): Double = {
+      def square(x: Double): Double = x*x
+      def good_enough(guess: Double) = (square(guess) - x).abs < 0.001
+      def improve(guess: Double) = (guess + x / guess) / 2
+      @tailrec
+      def iter(guess: Double): Double =
+        if (good_enough(guess)) guess
+        else iter(improve(guess))
+      iter(1.0)
+    }
+    def sqrt(x: Double) = solve(x)
+}
